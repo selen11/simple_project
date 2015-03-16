5ead038b6eba3a7d5f4fe4e092e9ec6fb4a85225
Anonym <selen.git@gmail.com>
2015-03-16 08:45:31 +0300

    Fix for houndci

--- spec/routing/flash_cards_routing_spec.rb
+++ spec/routing/flash_cards_routing_spec.rb
@@ -1,40 +1,39 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe FlashCardsController do
   it do
-    expect(get: '/').to route_to(
+    expect(get: "/").to route_to(
       controller: 'flash_cards',
       action: 'index')
   end
 
   it do
-    expect(get: '/').not_to route_to(
+    expect(get: "/").not_to route_to(
       controller: 'flash_cards',
       action: 'new')
   end
 
   it do
-    expect(post: '/').not_to route_to(
+    expect(post: "/").not_to route_to(
       controller: 'flash_cards',
       action: 'create')
   end
 
   it do
-    expect(get: '/flash_cards').to route_to(
+    expect(get: "/flash_cards").to route_to(
       controller: 'flash_cards',
       action: 'index')
   end
 
   it do
-    expect(get: '/flash_cards').not_to route_to(
+    expect(get: "/flash_cards").not_to route_to(
       controller: 'flash_cards',
       action: 'new')
   end
 
   it do
-    expect(post: '/flash_cards').not_to route_to(
+    expect(post: "/flash_cards").not_to route_to(
       controller: 'flash_cards',
       action: 'create')
   end
-
 end
