7d01c9112ff44c887095fc16bb45c696b4a26be7
Anonym <selen.git@gmail.com>
2015-03-16 08:40:44 +0300

    Add tests

--- spec/routing/flash_cards_routing_spec.rb
+++ spec/routing/flash_cards_routing_spec.rb
@@ -18,4 +18,23 @@ describe FlashCardsController do
       controller: 'flash_cards',
       action: 'create')
   end
+
+  it do
+    expect(get: '/flash_cards').to route_to(
+      controller: 'flash_cards',
+      action: 'index')
+  end
+
+  it do
+    expect(get: '/flash_cards').not_to route_to(
+      controller: 'flash_cards',
+      action: 'new')
+  end
+
+  it do
+    expect(post: '/flash_cards').not_to route_to(
+      controller: 'flash_cards',
+      action: 'create')
+  end
+
 end
