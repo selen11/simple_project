face3773c9487381c9b5a5259ed24869bf086a7e
Anonym <selen.git@gmail.com>
2015-03-18 04:42:25 +0300

    Delete old code

--- config/initializers/remove_this_sf_bs3_inputs.rb
+++ config/initializers/remove_this_sf_bs3_inputs.rb
@@ -7,6 +7,7 @@ inputs = %w(
   PasswordInput
   RangeInput
   StringInput
+  TextInput
 )
 
 inputs.each do |input_type|
@@ -20,12 +21,3 @@ inputs.each do |input_type|
 
   Object.const_set(input_type, new_class)
 end
-
-# add wysihtml5
-textarea = Class.new(SimpleForm::Inputs::TextInput) do
-  def input_html_classes
-    super.push('form-control wysihtml5')
-  end
-end
-
-Object.const_set('TextInput', textarea)
